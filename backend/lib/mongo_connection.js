c5e164c669da7283b9ae10a83c31535ba0bf481d
tylermcgraw <=>
2025-09-11 07:03:53 -0400

    Updated pg and mongo to connect to postgres rds and documentdb databases instead of ec2 instances. Using AWS Secret Manager to store username/password instead of .env

--- backend/lib/mongo_connection.js
+++ backend/lib/mongo_connection.js
@@ -1,10 +1,46 @@
+// Use this code snippet in your app.
+// If you need more information about configurations or implementing the sample code, visit the AWS docs:
+// https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/getting-started.html
+
+const {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} = require("@aws-sdk/client-secrets-manager");
+
+const secret_name = "docdb-mongo-login";
+
+const secret_client = new SecretsManagerClient({
+  region: "us-east-1",
+});
+
 const { MongoClient, ObjectId } = require("mongodb");
 const config = require("./config");
 
-const client = new MongoClient(config.MONGO_URI);
+async function getClient() {
+  let response;
+
+  try {
+    response = await secret_client.send(
+      new GetSecretValueCommand({
+        SecretId: secret_name,
+        VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+      })
+    );
+  } catch (error) {
+    // For a list of exceptions thrown, see
+    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    throw error;
+  }
+
+  const secret = JSON.parse(response.SecretString);
+  const mongo_uri = `mongodb://${secret.username}:${secret.password}@${secret.host}:${secret.port}/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`;
+
+  return new MongoClient(mongo_uri);
+}
 
 module.exports = {
   mongoInsertBody: async function (body) {
+    let client = await getClient();
     // Inserts the specified body as a document, then returns the document Id
     try {
       await client.connect();
@@ -21,6 +57,7 @@ module.exports = {
   },
 
   mongoGetBody: async function (docId) {
+    let client = await getClient();
     //Returns the body of the specified document
     try {
       await client.connect();
@@ -39,6 +76,7 @@ module.exports = {
   },
 
   mongoDeleteBody: async function (docId) {
+    let client = await getClient();
     //Deletes the request with the specified document Id, returns the deletion count
     try {
       await client.connect();
--- backend/lib/pg_connection.js
+++ backend/lib/pg_connection.js
@@ -1,9 +1,20 @@
+// If you need more information about configurations or implementing the sample code, visit the AWS docs:
+// https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/getting-started.html
+
+const {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} = require("@aws-sdk/client-secrets-manager");
+
+const secret_name = "postgres-rds-db";
+const secret_client = new SecretsManagerClient({
+  region: "us-east-1",
+});
+
 const { Client } = require("pg");
 const config = require("./config");
 const CONNECTION = {
   host: config.PG_HOST,
-  user: config.PG_USER,
-  password: config.PG_PASSWORD,
   database: config.PG_DATABASE,
 };
 
@@ -14,6 +25,25 @@ function logQuery(statement, parameters) {
 }
 
 module.exports = async function pgQuery(statement, ...parameters) {
+  let response;
+
+  try {
+    response = await secret_client.send(
+      new GetSecretValueCommand({
+        SecretId: secret_name,
+        VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+      })
+    );
+  } catch (error) {
+    // For a list of exceptions thrown, see
+    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    throw error;
+  }
+
+  const secret = JSON.parse(response.SecretString);
+  CONNECTION.user = secret.username;
+  CONNECTION.password = secret.password;
+
   let client = new Client(CONNECTION);
 
   await client.connect();
